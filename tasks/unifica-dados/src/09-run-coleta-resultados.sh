d3285b93d795dd27df0eb7cd57580a63d44d17f6
rdurl0 <durlo.raul@gmail.com>
2025-03-28 14:52:25 -0300

    script que roda coleta de itens-resultados-remanescentes

---
+++ tasks/unifica-dados/src/09-run-coleta-resultados.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Caminho para o script R
+SCRIPT="C:/Users/rdurl/OneDrive/Documentos/cesta-de-precos-pncp/src/ETL/coletores/coleta-resultados.R"
+
+# Parâmeto para o script R: (obrigatório) o caminho para um arquivo .csv que seja
+# um dataframe contendo uma coluna nomeada 'endpoint', indicando os endpoints de
+# itens das contratações coletados anteriormente.
+PARAMETRO_1="C:/Users/rdurl/OneDrive/Documentos/cesta-de-precos-pncp/coleta/itens/medicamentos.csv"
+
+# Executa o script R com o parâmetro
+Rscript "$SCRIPT" "$PARAMETRO_1"
