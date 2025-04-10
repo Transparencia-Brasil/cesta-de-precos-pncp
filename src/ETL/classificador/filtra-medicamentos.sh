ecb25007e995014f123256aa48c51fa0769f3d45
Raul de Sá Durlo <durlo.raul@gmail.com>
2025-04-10 12:22:24 -0300

    Update src/ETL/classificador/filtra-medicamentos.sh
    Co-authored-by: Jessé Oliveira <oliveirajesse50@gmail.com>

--- src/ETL/classificador/filtra-medicamentos.sh
+++ src/ETL/classificador/filtra-medicamentos.sh
@@ -4,7 +4,7 @@
 # SET DIR ----------------------------------------------------------------------
 
 # Garante que o script será executado a partir do diretório root do projeto
-cd "$(dirname "$0")/../../.." || exit 1
+cd "$(dirname "$0")/../../.." || { echo "Erro: Não foi possível acessar o diretório do projeto."; exit 1; }
 
 
 # PARÂMETROS DO BASH -----------------------------------------------------------
