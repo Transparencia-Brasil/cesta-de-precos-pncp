633bdf27c374b2a3e53e2fb95b4f9a61cac252d8
rdurl0 <durlo.raul@gmail.com>
2025-04-28 22:09:17 -0300

    BUG FIX na função `insere_tabela` - `unname(as.list(tb_catalogo[i, ]))`

--- src/ETL/loaders/utils.R
+++ src/ETL/loaders/utils.R
@@ -454,7 +454,8 @@ get_query <- function(qry, conectar = FALSE, quiet = FALSE) {
 insere_tabela <- function(con, tabela, consulta) {
   for (i in seq_len(nrow(tabela))) {
     tryCatch({
-      dbExecute(con, consulta, params = as.list(unname(tabela[i, ])))
+      params <- unname(as.list(tb_catalogo[i, ]))
+      dbExecute(con, consulta, params = params)
     }, error = function(e) {
       nome_tabela <- deparse(substitute(tabela))
       message(sprintf(
