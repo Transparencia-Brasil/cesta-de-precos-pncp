43864645d7587c81ddb38818bf1858170e233680
rdurl0 <durlo.raul@gmail.com>
2026-06-26 09:54:08 -0300

    feat: atualiza função para carregar itens homologados do banco de dados com novos campos e refatora caminho do arquivo

--- tasks/api-compras/src/R/00-load-item-homologado-from-db.R
+++ tasks/api-compras/src/R/00-load-item-homologado-from-db.R
@@ -6,6 +6,8 @@ library(DBI)
 
 source(here("src/ETL/loaders/utils.R"))
 
+PATH_ITEM_HOMOLOGADO <- here("tasks/api-compras/inputs/item-homologado.rds")
+
 # CONECTA-SE  COM O BD ----------------------------------------------------
 
 con <- conecta_bd_medicamentos_transparentes()
@@ -16,7 +18,10 @@ item_homologado <- get_query("
   select
   numero_controle_pncp,
   numero_item,
-  codigo_item_catalogo
+  codigo_item_catalogo,
+  ni_fornecedor,
+  valor_unitario_homologado,
+  quantidade_homologada,
   from item_homologado
 ") |>
   rename(
@@ -25,4 +30,5 @@ item_homologado <- get_query("
     codigoItemCatalogo = codigo_item_catalogo
   )
 
-saveRDS(item_homologado, here("tasks/api-compras/inputs/item-homologado.rds"))
+saveRDS(item_homologado, PATH_ITEM_HOMOLOGADO)
+readRDS(PATH_ITEM_HOMOLOGADO)
