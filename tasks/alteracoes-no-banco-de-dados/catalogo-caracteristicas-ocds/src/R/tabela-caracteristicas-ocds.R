052b978d4c66494df8c0b92066477326d9727402
rdurl0 <durlo.raul@gmail.com>
2026-07-01 11:34:20 -0300

    fix: inclui novo método para criação de características OCDS para filtro do site

---
+++ tasks/alteracoes-no-banco-de-dados/catalogo-caracteristicas-ocds/src/R/tabela-caracteristicas-ocds.R
@@ -0,0 +1,68 @@
+library(tidyverse)
+library(here)
+library(jsonlite)
+library(dotenv)
+load_dot_env()
+
+
+# :: FILE PATHS ----------------------------------------------------------------
+
+GITHUB_TOKEN <- Sys.getenv("GITHUB_TOKEN")
+
+REMOTE_DOCUMENT_JSON_URL <- "https://raw.githubusercontent.com/Transparencia-Brasil/medicine-extension-ocds/refs/heads/main/tasks/documento-ocds/outputs/documento-ocds.json?token="
+
+INPUT_PATH <- paste0(REMOTE_DOCUMENT_JSON_URL, GITHUB_TOKEN)
+
+OUTPUT_PATH <- here("tasks/alteracoes-no-banco-de-dados/catalogo-caracteristicas-ocds/outputs/tabela-mapeamento-ocds.csv")
+
+UTILS_PATH <- here("tasks/alteracoes-no-banco-de-dados/catalogo-caracteristicas-ocds/src/R/utils.R")
+
+# :: MAIN ----------------------------------------------------------------------
+
+source(UTILS_PATH)
+
+# Parse the OCDS JSON data
+ocds_json <- fromJSON(INPUT_PATH)
+
+# Tidy the OCDS JSON data and extract relevant information
+ocds <- tidy_ocds_json(ocds_json) |>
+  mutate(
+    strength = map_chr(activeIngredients, extrai_strength),
+    activeIngredients = map_chr(activeIngredients, extrai_activeIngredients),
+    immediateContainer = map_chr(immediateContainer, extrai_immediateContainer)
+  ) |>
+  select(-attributes)
+
+# Create the final data frame with the relevant information and write it to a CSV file
+caracteristicas_ocds <- ocds |>
+  mutate(
+    across(
+      c(dosageForm, administrationRoute, immediateContainer, activeIngredients, strength),
+      \(x) replace_na(x, "")
+    ),
+    caracteristicas_ocds = pmap(
+      list(dosageForm, administrationRoute, immediateContainer, activeIngredients, strength),
+      function(dosageForm, administrationRoute, immediateContainer, activeIngredients, strength) {
+        list(
+          list(nomeCaracteristica = "dosageForm", nomeValorCaracteristica = dosageForm),
+          list(nomeCaracteristica = "administrationRoute", nomeValorCaracteristica = administrationRoute),
+          list(nomeCaracteristica = "immediateContainer", nomeValorCaracteristica = immediateContainer),
+          list(nomeCaracteristica = "activeIngredients", nomeValorCaracteristica = activeIngredients),
+          list(nomeCaracteristica = "strength", nomeValorCaracteristica = strength)
+        )
+      }
+    )
+  )
+
+# Wrap the caracteristicas_ocds column in a list column and convert it to JSON
+caracteristicas_ocds <- caracteristicas_ocds |>
+  mutate(
+    caracteristicas_ocds = map(caracteristicas_ocds, discard, ~ .x$nomeValorCaracteristica == ""),
+    caracteristicas_ocds = map(caracteristicas_ocds, toJSON)
+  )
+
+# Write the final data frame to a CSV file
+caracteristicas_ocds |>
+  select(codigo_item, caracteristicas_ocds) |>
+  mutate(caracteristicas_ocds = as.character(caracteristicas_ocds)) |>
+  write_csv(OUTPUT_PATH)
---
+++ tasks/alteracoes-no-banco-de-dados/catalogo-caracteristicas-ocds/src/R/utils.R
@@ -0,0 +1,126 @@
+
+# :: FUNCTIONS -----------------------------------------------------------------
+
+#' Tidy the OCDS JSON data
+#'
+#' @param ocds_json A list containing the OCDS JSON data
+#' @return A tidy data frame containing the relevant information from the OCDS JSON data
+#'
+tidy_ocds_json <- function(ocds_json) {
+  ocds <- ocds_json |>
+    enframe() |>
+    pivot_wider() |>
+    select(releases) |>
+    unnest(releases) |>
+    select(tender) |>
+    unnest(tender) |>
+    select(items) |>
+    unnest(items)
+
+  ocds <- ocds |>
+    unnest_wider(classification, names_sep = "_") |>
+    select(
+      id_item = id,
+      description = description,
+      codigo_item = classification_id,
+      dosageForm = dosageForm,
+      administrationRoute = administrationRoute,
+      activeIngredients = activeIngredients,
+      attributes = attributes,
+      immediateContainer = immediateContainer
+    )
+  ocds
+}
+
+#' Extrai a força do medicamento a partir dos ingredientes ativos
+#'
+#' @param active_ingredients Um data frame contendo os ingredientes ativos do medicamento
+#' @return Uma string contendo a força do medicamento, ou NA se não houver força disponível
+#'
+extrai_strength <- function(active_ingredients) {
+  if (!"strength" %in% names(active_ingredients)) return(NA_character_)
+
+  strength <- active_ingredients |>
+    select(strength) |>
+    unnest(strength, keep_empty = TRUE)
+
+  if (!"value" %in% names(strength)) return(NA_character_)
+
+  if ("unit" %in% names(strength)) {
+    strength <- strength |>
+      unnest(unit, keep_empty = TRUE)
+  } else {
+    strength <- strength |>
+      mutate(id = NA_character_)
+  }
+
+  if (!"id" %in% names(strength)) {
+    strength <- strength |>
+      mutate(id = NA_character_)
+  }
+
+  strength <- strength |>
+    filter(!is.na(value)) |>
+    mutate(
+      value = as.character(value),
+      id = replace_na(as.character(id), ""),
+      strength = str_squish(paste(value, id))
+    ) |>
+    pull(strength)
+
+  if (length(strength) == 0) {
+    return(NA_character_)
+  }
+
+  paste(strength, collapse = " + ")
+}
+
+#' Extrai os nomes dos ingredientes ativos a partir do data frame de ingredientes ativos
+#'
+#' @param active_ingredients Um data frame contendo os ingredientes ativos do medicamento
+#' @return Uma string contendo os nomes dos ingredientes ativos, ou NA se não houver nomes
+#'
+extrai_activeIngredients <- function(active_ingredients) {
+  if (!"name" %in% names(active_ingredients)) return(NA_character_)
+
+  active_ingredients_name <- active_ingredients |>
+    filter(!is.na(name)) |>
+    pull(name)
+
+  if (length(active_ingredients_name) == 0) return(NA_character_)
+
+  active_ingredients_name_main <- active_ingredients_name[[1]]
+  active_ingredients_name_associated <- setdiff(active_ingredients_name, active_ingredients_name_main)
+
+  active_ingredients_name <- paste(
+    active_ingredients_name_main,
+    paste(active_ingredients_name_associated, collapse = ", "),
+    sep = "; "
+  )
+
+  str_replace(active_ingredients_name, "; $", "")
+}
+
+#' Extrai o recipiente imediato a partir do data frame de recipiente imediato
+#'
+#' @param immediate_container Um data frame contendo o recipiente imediato do medicamento
+#' @return Uma string contendo o recipiente imediato, ou NA se não houver nome
+#'
+extrai_immediateContainer <- function(immediate_container) {
+  if (is.null(immediate_container)) {
+    return(NA_character_)
+  }
+  if (!"name" %in% names(immediate_container)) {
+    return(NA_character_)
+  }
+
+  immediate_container_name <- immediate_container |>
+    filter(!is.na(name)) |>
+    pull(name)
+
+  if (length(immediate_container_name) == 0) {
+    return(NA_character_)
+  }
+
+  paste(immediate_container_name, collapse = ", ")
+}
