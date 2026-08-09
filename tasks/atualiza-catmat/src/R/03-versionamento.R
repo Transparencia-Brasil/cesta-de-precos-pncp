47cdd436cc35a7f6c80d22df68c3bd02f3f631f0
rdurl0 <durlo.raul@gmail.com>
2026-08-09 10:40:01 -0300

    feat: adiciona suporte para geração e movimentação de arquivos do classificador no versionamento

--- tasks/atualiza-catmat/src/R/03-versionamento.R
+++ tasks/atualiza-catmat/src/R/03-versionamento.R
@@ -35,6 +35,7 @@ OLD_DIR <- here(CATMAT_DIR, "OLD", OLD_VERSION)
 
 OLD_FILE_OCDS <- sprintf("tabela-mapeamento-ocds-%s.csv", OLD_VERSION)
 OLD_FILE_CATALOGO <- sprintf("catmat-%s.rds", OLD_VERSION)
+OLD_FILE_CLASSIFICADOR <- sprintf("catmat-%s.csv", OLD_VERSION)
 
 # cria diretório OLD
 if (!dir.exists(OLD_DIR)) dir.create(OLD_DIR, recursive = TRUE)
@@ -43,6 +44,26 @@ if (!dir.exists(OLD_DIR)) dir.create(OLD_DIR, recursive = TRUE)
 
 NEW_FILE_OCDS <- sprintf("tabela-mapeamento-ocds-%s.csv", NEW_VERSION)
 NEW_FILE_CATALOGO <- sprintf("catmat-%s.rds", NEW_VERSION)
+NEW_FILE_CLASSIFICADOR <- sprintf("catmat-%s.csv", NEW_VERSION)
+
+
+# :: GERA CATÁLOGO DO CLASSIFICADOR -------------------------------------------
+
+CATMAT_CLASSIFICADOR_COLUNAS <- c(
+  "codigo_pdm",
+  "nome_pdm",
+  "codigo_br",
+  "nome_item"
+)
+
+CAMINHO_CATALOGO_ATUALIZADO <- here(ATUALIZACAO_DIR, "catmat.rds")
+CAMINHO_CATALOGO_CLASSIFICADOR <- here(ATUALIZACAO_DIR, "catmat.csv")
+
+catalogo_atualizado <- readRDS(CAMINHO_CATALOGO_ATUALIZADO)
+
+catalogo_atualizado |>
+  select(all_of(CATMAT_CLASSIFICADOR_COLUNAS)) |>
+  write_csv(CAMINHO_CATALOGO_CLASSIFICADOR)
 
 
 # :: MOVIMENTA ARQUIVOS ANTIGOS ------------------------------------------------
@@ -50,16 +71,20 @@ NEW_FILE_CATALOGO <- sprintf("catmat-%s.rds", NEW_VERSION)
 pivot_old <- tibble(
   from = c(
     here(CATMAT_DIR, OLD_FILE_OCDS),
-    here(CATMAT_DIR, OLD_FILE_CATALOGO)
+    here(CATMAT_DIR, OLD_FILE_CATALOGO),
+    here(CATMAT_DIR, OLD_FILE_CLASSIFICADOR)
   ),
   to = c(
     here(OLD_DIR, OLD_FILE_OCDS),
-    here(OLD_DIR, OLD_FILE_CATALOGO)
+    here(OLD_DIR, OLD_FILE_CATALOGO),
+    here(OLD_DIR, OLD_FILE_CLASSIFICADOR)
   )
 )
 
 # move arquivos antigos para o diretório OLD
-pmap(pivot_old, file.copy)
+pivot_old |>
+  filter(file.exists(from)) |>
+  pmap(file.copy)
 
 # deleta arquivos antigos do diretório principal
 unlink(pivot_old$from)
@@ -70,11 +95,13 @@ unlink(pivot_old$from)
 pivot_new <- tibble(
   from = c(
     here(ATUALIZACAO_DIR, "tabela-mapeamento-ocds.csv"),
-    here(ATUALIZACAO_DIR, "catmat.rds")
+    here(ATUALIZACAO_DIR, "catmat.rds"),
+    CAMINHO_CATALOGO_CLASSIFICADOR
   ),
   to = c(
     here(CATMAT_DIR, NEW_FILE_OCDS),
-    here(CATMAT_DIR, NEW_FILE_CATALOGO)
+    here(CATMAT_DIR, NEW_FILE_CATALOGO),
+    here(CATMAT_DIR, NEW_FILE_CLASSIFICADOR)
   )
 )
 
